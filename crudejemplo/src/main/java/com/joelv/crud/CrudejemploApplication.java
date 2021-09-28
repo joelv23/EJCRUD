65586e62ef1f9cd0d0e5430e4511f1318a8b9e8b
joelv23 <91219456+joelv23@users.noreply.github.com>
2021-09-28 00:18:32 -0300

    agregue el proyecto

---
+++ crudejemplo/src/main/java/com/joelv/crud/CrudejemploApplication.java
@@ -0,0 +1,13 @@
+package com.joelv.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudejemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudejemploApplication.class, args);
+    }
+
+}
---
+++ crudejemplo/src/test/java/com/joelv/crud/CrudejemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelv.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudejemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
